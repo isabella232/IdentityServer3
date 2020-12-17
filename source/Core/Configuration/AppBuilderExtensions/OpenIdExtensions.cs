c237e6787653a64ce6ac9620c29d42f98073a429
rstefko <roman.stefko@eway-crm.com>
2020-12-17 15:55:31 +0100

    Added Azure AD connect extension

---
+++ source/Core/Configuration/AppBuilderExtensions/OpenIdExtensions.cs
@@ -0,0 +1,32 @@
+﻿using IdentityServer3.Core.Extensions;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer3.Core.Configuration.AppBuilderExtensions
+{
+    public static class OpenIdExtensions
+    {
+        public static IAppBuilder UseAzureAdAuthentication(this IAppBuilder app, AzureAdAuthenticationOptions options)
+        {
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            return app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            {
+                AuthenticationType = "aad",
+                Caption = "AzureAD",
+                Scope = "openid profile",
+                SignInAsAuthenticationType = options.SignInAsAuthenticationType,
+
+                Authority = $"https://login.microsoftonline.com/{options.TenantId}",
+                ClientId = options.ClientId,
+                RedirectUri = options.RedirectUri,
+            });
+        }
+    }
+}
---
+++ source/Core/Configuration/AzureAdAuthenticationOptions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer3.Core.Configuration
+{
+    public class AzureAdAuthenticationOptions
+    {
+        /// <summary>
+        /// Gets or sets authentication type.
+        /// </summary>
+        public string SignInAsAuthenticationType { get; set; }
+
+        /// <summary>
+        /// Gets or sets Azure AD client ID.
+        /// </summary>
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// Get or sets Azure AD tenant ID.
+        /// </summary>
+        public string TenantId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the redirect URI.
+        /// </summary>
+        public string RedirectUri { get; set; }
+    }
+}
